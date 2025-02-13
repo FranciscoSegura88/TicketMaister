85fa89fda0e44c262b1b5aaa0b77f7e8d4326077
FranciscoSegura88 <125767908+FranciscoSegura88@users.noreply.github.com>
2025-02-12 21:30:02 -0600

    guardando progreso

---
+++ backend/spring-boot-microservice-1-eventos/src/main/java/com/eventos/spring_boot_microservice_1_eventos/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.eventos.spring_boot_microservice_1_eventos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    String login() {
+        return "login";
+    }
+}
